7ba7a2e0fccd0c2391ae54cba7c2e13d13e8dafe
Philipp Sieweck <psi@informatik.uni-kiel.de>
2016-11-04 15:20:47 +0100

    NodeId implements Hash

--- src/node_id.rs
+++ src/node_id.rs
@@ -1,6 +1,7 @@
 #![allow(expl_impl_clone_on_copy)]
 
 use super::NodeId;
+use std::hash::{Hash, Hasher};
 
 impl<T> Copy for NodeId<T> { }
 impl<T> Clone for NodeId<T> {
@@ -11,6 +12,12 @@ impl<T> Eq for NodeId<T> { }
 impl<T> PartialEq for NodeId<T> {
     fn eq(&self, other: &Self) -> bool {
         self.tree_id == other.tree_id && self.index == other.index
+    }
+}
+
+impl<T> Hash for NodeId<T> {
+    fn hash<H: Hasher>(&self, state: &mut H) {
+        self.index.hash(state);
     }
 }
 
